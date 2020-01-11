1b4a7c2d3dfc05d8239b4fdcb7ecf2b307ffad0a
Musa <mussesemou99@gmail.com>
2020-01-11 20:01:56 +0100

    Added aliases

--- helpers/.musa11971-helpers.sh
+++ helpers/.musa11971-helpers.sh
@@ -4,3 +4,9 @@
 function sublime() {
   open $1 -a "Sublime Text"
 }
+
+# PHPUnit alias
+alias pu='phpunit --testdox'
+
+# Laravel migrate:fresh --seed alias
+alis mfs='php artisan migrate:fresh --seed'
