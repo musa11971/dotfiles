5cd2a61e3b2f554beb6852cf8fa0bed409959b35
dakyskye <lashakanteladze339@protonmail.com>
2020-01-30 17:17:31 +0400

    some improvements to the shell scripts

--- helpers/.musa11971-helpers.sh
+++ helpers/.musa11971-helpers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # musa11971-helpers command
-function musa {
-	if [ -z $1 ] || [ $1 = 'help' ] || [ $1 = '' ]; then
+musa() {
+	if [ -z "$1" ] || [ "$1" = 'help' ] || [ "$1" = '' ]; then
 		# Help command
 		echo -e "       📄 \e[1mmusa11971-helpers"
 		echo -e "  \e[1mhttps://github.com/musa11971/dotfiles\e[0m\n"
@@ -17,7 +17,7 @@ function musa {
 		echo -e "  \e[32mmfs\e[0m\t\tRuns php artisan migrate:fresh --seed"
 		echo -e "  \e[32mfinder\e[0m\tOpens the current folder in Finder"
 		echo -e "  \e[32mrepo\e[0m\t\tOpens the current git repository in the browser"
-	elif [ $1 = 'update' ]; then
+	elif [ "$1" = 'update' ]; then
 		# Update command
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/musa11971/dotfiles/master/helpers/install-helpers.sh)"
 	else
@@ -26,8 +26,8 @@ function musa {
 }
 
 # Open file or folder in Sublime Text
-function sublime() {
-  open $1 -a "Sublime Text"
+sublime() {
+	open "$1" -a "Sublime Text"
 }
 
 # PHPUnit alias
