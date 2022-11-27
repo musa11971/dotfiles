d956cd62ccf010215be6f65bf003d4667f2aab37
musa11971 <21341801+musa11971@users.noreply.github.com>
2022-11-27 17:43:38 +0100

    Suppress login banner

--- helpers/install-helpers.sh
+++ helpers/install-helpers.sh
@@ -5,6 +5,9 @@ echo "Installing musa11971 helpers..."
 # Remove the helpers if they were already installed
 rm ~/.musa11971-helpers.sh 2> /dev/null
 
+# Create hushlogin file to suppress login banner
+touch ~/.hushlogin
+
 # Download the helpers
 curl -s https://raw.githubusercontent.com/musa11971/dotfiles/master/helpers/.musa11971-helpers.sh --output ~/.musa11971-helpers.sh > /dev/null
 
