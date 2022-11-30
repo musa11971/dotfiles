771929f212ec997015e0f6696f9f93faec1ab386
Musa <21341801+musa11971@users.noreply.github.com>
2022-11-30 20:03:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sh helpers/install-helpers.sh
 
 # Check for Homebrew and install if we don't have it
 if test ! "$(which brew)"; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew recipes
